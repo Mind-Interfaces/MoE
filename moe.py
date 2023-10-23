e59e30ea0c0674f6b04da25c05fa077f21a03a5f
Mind-Interfaces <94399418+Mind-Interfaces@users.noreply.github.com>
2023-10-23 06:11:40 -0400

    Update moe.py

--- moe.py
+++ moe.py
@@ -1,32 +1,38 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn.functional as fun
+
+
 class SimpleExpert(nn.Module):
     def __init__(self):
         super(SimpleExpert, self).__init__()
         self.fc1 = nn.Linear(10, 10)
 
-    def forward(self, x):
-        return F.relu(self.fc1(x))
+    def forward(self, user_input):
+        return fun.relu(self.fc1(user_input))
+
+
 class GatingMechanism(nn.Module):
     def __init__(self, num_experts):
         super(GatingMechanism, self).__init__()
         self.fc = nn.Linear(10, num_experts)
 
-    def forward(self, x):
-        return F.softmax(self.fc(x), dim=1)
+    def forward(self, user_input):
+        return fun.softmax(self.fc(user_input), dim=1)
+
+
 class MixtureOfExperts(nn.Module):
     def __init__(self, num_experts):
         super(MixtureOfExperts, self).__init__()
         self.experts = nn.ModuleList([SimpleExpert() for _ in range(num_experts)])
         self.gating = GatingMechanism(num_experts)
 
-    def forward(self, x):
+    def forward(self, user_input):
         # Get gating weights
-        gating_weights = self.gating(x)
+        gating_weights = self.gating(user_input)
 
         # Forward pass through each expert
-        expert_outputs = [expert(x) for expert in self.experts]
+        expert_outputs = [expert(user_input) for expert in self.experts]
 
         # Combine the experts' outputs based on the gating weights
         combined_output = 0
@@ -34,15 +40,17 @@ class MixtureOfExperts(nn.Module):
             combined_output += gating_weights[:, i:i+1] * expert_output
 
         return combined_output
+
+
 # Number of experts
 num_experts = 16
 
 # Instantiate MoE
 moe = MixtureOfExperts(num_experts)
 
-# Sample input
-x = torch.rand(5, 10)
+# Sample input for example. Batch size = 5, input size = 10
+user_input = torch.rand(5, 10)
 
 # Forward pass
-output = moe(x)
+output = moe(input)
 print(output)
